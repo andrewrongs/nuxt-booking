0ecd502adbe717edf6a94263bdde1935e2f9ce97
Andrew <fter2257@gmail.com>
2025-05-08 14:06:39 +0800

    fix: tailwind 斷點設置

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,15 @@ export default {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
-    screens: {
-      // 'ph': '320px',
+    extend: {
+      screens: {
+        'ph': '320px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
     },
   },
   plugins: [],
